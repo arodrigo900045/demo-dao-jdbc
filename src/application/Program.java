c1104e7e1d7949a7453b4be87097afc24278aead
arodrigoo900045 <adilsonmissionario@gmail.com>
2021-05-10 23:58:07 +0100

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 	public static void main(String[] args)
 	{
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		//System.out.println(obj);
         
 		Seller seller = new Seller(21,"Rodrigo", "rodrigo@prodel.co.ao", new Date(), 356000.00, obj);
 		System.out.println(seller);
