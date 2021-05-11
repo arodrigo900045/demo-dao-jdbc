b567ad3bb7f65c087dca49f25f7e50d30a90f82b
arodrigoo900045 <adilsonmissionario@gmail.com>
2021-05-11 01:01:51 +0100

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
 	public static void main(String[] args)
 	{
-		Department obj = new Department(1, "Books");
-		//System.out.println(obj);
-        
-		Seller seller = new Seller(21,"Rodrigo", "rodrigo@prodel.co.ao", new Date(), 356000.00, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
